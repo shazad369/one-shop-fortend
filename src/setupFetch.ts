cad323f35e1c8958edbfd00a399e04dc17b41962
shazad369 <shazadahamed571@gmail.com>
2026-07-01 22:28:25 +0600

    Update setupFetch.ts

--- src/setupFetch.ts
+++ src/setupFetch.ts
@@ -2,5 +2,11 @@ const originalFetch = window.fetch;
 window.fetch = (input: RequestInfo | URL, init?: RequestInit) => {
   const headers = new Headers(init?.headers || {});
   headers.set('localtonet-skip-warning', 'true');
-  return originalFetch(input, { ...init, headers });
+  
+  return originalFetch(input, { 
+    ...init, 
+    headers,
+    mode: 'cors',
+    credentials: 'omit'
+  });
 };
