185a38090718d26fab835eb0f5210d4ee3ece999
shazad369 <shazadahamed571@gmail.com>
2026-07-06 12:25:43 +0600

    Create sitemap.xml.js

---
+++ functions/sitemap.xml.js
@@ -0,0 +1,22 @@
+export async function onRequest(context) {
+  // তোমার backend theke shob product fetch koro
+  const response = await fetch('https://your-tailscale-backend-url/shopdata');
+  const products = await response.json();
+
+  const urls = products.map(p => `
+    <url>
+      <loc>https://oneshop.pre.bd/product/${p._id}</loc>
+      <priority>0.8</priority>
+    </url>
+  `).join('');
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>https://oneshop.pre.bd/</loc><priority>1.0</priority></url>
+  ${urls}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: { "Content-Type": "application/xml" }
+  });
+}
