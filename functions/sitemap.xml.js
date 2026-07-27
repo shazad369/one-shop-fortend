8da36e5405bd6306112c5702f1e887a345ba6998
shazad369 <shazadahamed571@gmail.com>
2026-07-27 15:03:55 +0600

    Reduce cache duration for sitemap response

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -138,7 +138,7 @@ ${allUrls
     headers: {
       "Content-Type": "application/xml",
       // ✅ 7 din cache - cron worker proti 7 din e nijei force-refresh kore
-      "Cache-Control": "public, max-age=604800, s-maxage=604800",
+      "Cache-Control": "public, max-age=60, s-maxage=60",
     },
   });
 
